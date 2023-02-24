b89d957780cd9d05c952c71a2f1a65ffd03a680d
Hasnat-digitizal <hasnat.khan@digitizal.com>
2023-02-24 16:40:51 +0500

    new

--- routes/web.php
+++ routes/web.php
@@ -32,13 +32,7 @@ Route::resources([
 //front
 Route::get('/',[HomeController::class,'index'])->name('home.index');
 Route::get('/course/detail/{id}',[CourseDetailController::class,'courseDetails'])->name('course.details');
-<<<<<<< HEAD
 Route::get('/getCourseData/{dataId}',[CourseDetailController::class,'courseData']);
-=======
-
-Route::get('/getCourseData/{dataId}',[CourseDetailController::class,'courseData']);
-
->>>>>>> d1cdc6f604443362f436554a73f1db5b9aa55030
 Route::get('booking',[BookingController::class,'index'])->name('course.booking');
 route::get('about',function(){
     return view('front.about');
@@ -46,14 +40,9 @@ route::get('about',function(){
 route::get('contact',function(){
     return view('front.contact');
 });
-
-
-<<<<<<< HEAD
 Route::get('/getlocation/{dataId}',[BookingController::class,'getlocation']);
 Route::get('/getsession/{course_id}/{city_id}',[BookingController::class,'getsession']);
 Route::get('/getseats/{id}',[BookingController::class,'getseats']);
 
 
-=======
->>>>>>> d1cdc6f604443362f436554a73f1db5b9aa55030
 
